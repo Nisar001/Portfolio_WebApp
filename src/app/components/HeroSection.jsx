6b6e6c4cb278eab4b725d762f484d00506379846
Nisar Khan <khannisarnk0786@gmail.com>
2023-11-04 13:50:57 +0530

    Hero Section is completed

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,19 +1,45 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
    <section>
-          <div className="grid grid-col-1 lg:grid-cols-12">
-            <div className="col-span-7 place-self-center">
-               <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Nisar</h1>
-               <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">I have a good background in Frontend, MERN (MongoDB, Express, React, Node.js), 
+          <div className="grid grid-col-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+               <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-green-500 ">Hello, I'm {" "}
+              </span>
+              <br></br>
+               <TypeAnimation
+                  sequence={[
+                  'Nisar',
+                  1000, 
+                  'Web Developer',
+                  1000,
+                  'React Developer',
+                  1000,
+                  'Designer',
+                  1000
+               ]}
+               wrapper="span"
+               speed={50}
+               repeat={Infinity}
+               /></h1>
+               <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">I have a good background in Frontend, MERN (MongoDB, Express, React, Node.js), 
                and Java, and I am an enthusiastic student of Computer Science. My areas of competence 
                include database management, design, and web application development. 
                </p>
                <div>
-                  <button>Hire Me !</button>
-                  <button>Download Resume</button>
+                  <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-yellow-500 hover:bg-slate-200 text-white">
+                  Hire Me !
+                  </button>
+                  <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-yellow-500 hover:bg-slate-800 text-white mt-3">
+                  <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                     Download Resume
+                  </span>
+                  </button>
                </div>
             </div>
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
