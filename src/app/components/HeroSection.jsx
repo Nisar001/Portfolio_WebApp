42181636ea680215d076bbb78d3a24f7fce90dca
Nisar Khan <khannisarnk0786@gmail.com>
2024-01-04 16:08:24 +0530

    skill section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -34,7 +34,7 @@ const HeroSection = () => {
                   </button>
                   <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-yellow-500 to-green-500 hover:bg-slate-800 text-white mt-3">
                   <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                     Download Resume
+                     <a href="/files/Nisar_khan Resume_NW.pdf" download>Download Resume</a>
                   </span>
                   </button>
                </div>
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -3,59 +3,60 @@ import React, { useState } from 'react'
 import ProjectCard from './ProjectCard'
 import ProjectTag from './ProjectTag';
 
+
 const projectsData = [
    {
       id: 1,
-      title: "",
-      description: "",
-      image: "",
+      title: "V-Room ",
+      description: "Video Conferencing Web Application based on React, Redux, Typescript and Firebase also include the Zego-Cloud.",
+      image: "/images/projects/vroom.png",
       tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
+      gitUrl: "https://github.com/Nisar001/V_Room",
+      previewUrl: "https://github.com/Nisar001/V_Room",
    },
    {
       id: 2,
-      title: "",
-      description: "",
-      image: "",
-      tag: ["All", "Java/Python"],
-      gitUrl: "/",
-      previewUrl: "/",
+      title: "3D Mario",
+      description: "A mario game in 3d which is developed on unity software and c# is used for scripting.",
+      image: "/images/projects/marioImage.jpg",
+      tag: ["All", "Other"],
+      gitUrl: "https://github.com/Nisar001/mario-game-3D",
+      previewUrl: "https://github.com/Nisar001/mario-game-3D",
    },
    {
       id: 3,
-      title: "",
-      description: "",
-      image: "",
+      title: "PortFolio Web Application",
+      description: "Self Portfolio using NextJS and modern technologies.",
+      image: "/images/projects/portfolio.png",
       tag: ["All", "Web"],
-      gitUrl: "/",
+      gitUrl: "https://github.com/Nisar001/portfolio_webapp",
       previewUrl: "/",
    },
    {
       id: 4,
-      title: "",
-      description: "",
-      image: "",
-      tag: ["All", "Java/Python"],
-      gitUrl: "/",
+      title: "Facial Expression Detection",
+      description: "A Python language system based on super-vised learning which detect the face of the user and gives the output.",
+      image: "/images/projects/facialexp.png",
+      tag: ["All", "Other"],
+      gitUrl: "https://github.com/Nisar001/facial-expression-detection",
       previewUrl: "/",
    },
    {
       id: 5,
-      title: "",
-      description: "",
-      image: "",
+      title: "Job Portal",
+      description: "A web application based on full MERN Stack technology with cloud database and fully responsiveness.",
+      image: "/images/projects/jobPortal.png",
       tag: ["All", "Web"],
-      gitUrl: "/",
+      gitUrl: "https://github.com/Nisar001/Job_Portal",
       previewUrl: "/",
    },
    {
       id: 6,
-      title: "",
-      description: "",
-      image: "",
-      tag: ["All", "Java/Python"],
-      gitUrl: "/",
+      title: "E-Commerce Platform",
+      description: "ECommerce web application based on Full MERN stack with cloud database.",
+      image: "/images/projects/ECOME.jpg",
+      tag: ["All", "Web"],
+      gitUrl: "https://github.com/Nisar001/E-Commerce-Web-App",
       previewUrl: "/",
    },
 ]
@@ -88,8 +89,8 @@ const ProjectSection = () => {
          />
          <ProjectTag 
          onClick={handleTagChange} 
-         name="Java/Python" 
-         isSelected={tag === "Java/Python"}
+         name="Other" 
+         isSelected={tag === "Other"}
          />
       </div>
       <div className="grid md:grid-cols-3 gap-8 md:gap-12">
@@ -98,7 +99,7 @@ const ProjectSection = () => {
          key={project.id} 
          title={project.title} 
          description={project.description} 
-         imgUrl={project.imgUrl}
+         imgUrl={project.image}
          gitUrl={project.gitUrl}
          previewUrl={project.previewUrl}/>)
       }
