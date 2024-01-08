bd2886fe748d6fd651c444549e883474fbbf7caa
Nisar Khan <khannisarnk0786@gmail.com>
2024-01-08 12:31:26 +0530

    Skill section is ready

--- src/app/components/SkillCard.jsx
+++ src/app/components/SkillCard.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
-import Link from 'next/link'
 
 const SkillCard = ({Simg, Sname, Status}) => {
   return (
    <div>
       <div 
-         className="h-52 md:h-72 rounded-t-xl relative group" 
-         style={{background: `url()`, backgroundSize: "cover"}}
+         className="h-40 md:h-40 w-30 md:w-30 rounded-t-xl relative group" 
+         style={{background: `url(${Simg})`, backgroundSize: "cover"}}
          >
-         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
-            <Link href={""} className="h-14 w-14 m-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
-            </Link>
-            <Link href={""} className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link">
-            </Link>
-         </div>
       </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
-         <h5 className="text-xl font-semibold mb-2">{}</h5>
-         <p className="text-[#ADB7BE]">{}</p>
+      <div className="text-white rounded-b-l mt-3 bg-[#181818] py-4 px-4">
+         <h5 className="text-xl font-semibold mb-2">{Sname}</h5>
+         <p className="text-[#ADB7BE]">Status -  {Status}</p>
       </div>
    </div>
   )
--- src/app/components/SkillSection.jsx
+++ src/app/components/SkillSection.jsx
@@ -1,9 +1,116 @@
 import React from 'react'
 import SkillCard from './SkillCard'
 
+const SkillsData = [
+   {
+      title: "HTML",
+      image: "/images/technologies/html.png",
+      Status: "Advanced",
+   },
+   {
+      title: "CSS",
+      image: "/images/technologies/css.png",
+      Status: "Advanced",
+   },
+   {
+      title: "Bootstrap",
+      image: "/images/technologies/bootstrap.jpg",
+      Status: "Advanced",
+   },
+   {
+      title: "Elastic UI",
+      image: "/images/technologies/elasticUI.png",
+      Status: "Intermediate",
+   },
+   {
+      title: "JavaScript ES6",
+      image: "/images/technologies/js.png",
+      Status: "Advanced",
+   },
+   {
+      title: "TypeScript",
+      image: "/images/technologies/typescript.png",
+      Status: "Intermediate",
+   },
+   {
+      title: "React JS",
+      image: "/images/technologies/react.png",
+      Status: "Advanced",
+   },
+   {
+      title: "Redux JS",
+      image: "/images/technologies/redux.png",
+      Status: "Intermediate",
+   },
+   {
+      title: "Express JS",
+      image: "/images/technologies/express-logo.png",
+      Status: "Intermediate",
+   },
+   {
+      title: "Node JS",
+      image: "/images/technologies/node.png",
+      Status: "Intermediate",
+   },
+   {
+      title: "JAVA",
+      image: "/images/technologies/java.png",
+      Status: "Intermediate",
+   },
+   {
+      title: "Python",
+      image: "/images/technologies/python.jpeg",
+      Status: "Intermediate",
+   },
+   {
+      title: "Mongo DB",
+      image: "/images/technologies/mongodb1.png",
+      Status: "Advanced",
+   },
+   {
+      title: "SQL",
+      image: "/images/technologies/sql.png",
+      Status: "Advanced",
+   },
+   {
+      title: "DBMS",
+      image: "/images/technologies/dbms.png",
+      Status: "Advanced",
+   },
+   {
+      title: "GIT",
+      image: "/images/technologies/git.png",
+      Status: "Advanced",
+   },
+   {
+      title: "GitHUb",
+      image: "/images/technologies/github.png",
+      Status: "Advanced",
+   },
+   {
+      title: "Ionic (Application FW)",
+      image: "/images/technologies/ionic-logo.png",
+      Status: "Learning",
+   },
+]  
 const SkillSection = () => {
   return (
-    <div>SkillSection</div>
+   <>
+   <h2 className="text-center text-4xl font-bold text-white mt-4">
+      My Skills
+      </h2>
+      <br/><br/>
+    <div className="grid md:grid-cols-6 gap-2 md:gap-5">
+      {
+         SkillsData.map((skills)=><SkillCard  
+         Sname={skills.title}  
+         Simg={skills.image}
+         Status={skills.Status}
+         />)
+      }
+      </div>
+      <br/><br/>
+   </>
   )
 }
 
